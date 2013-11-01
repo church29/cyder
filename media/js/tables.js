506808b12df3c16f6b0dff9fd5b3466f70a30a24
church29 <joshchurch29@gmail.com>
2013-10-31 17:05:37 -0700

    Initial js for batch update

--- media/js/tables.js
+++ media/js/tables.js
@@ -1,8 +1,52 @@
+function enableBatchUpdate() {
+    //Remove batch update mode
+    if ($('#enable-eg').length) {
+        $('#enable-eg').remove()
+    };
+    // Remove Action column.
+    if ($('th:contains("Actions")')) {
+        $('th:contains("Actions")').remove();
+        $('td:last-child').remove();
+    }
+
+    // Remove Info column.
+    if ($('th:contains("Info")')) {
+        $('th:contains("Info")').remove();
+        $('td:first-child').remove();
+    }
+    $('#egtable').find('td, th').each(function (i, td) {
+        var $td = $(td);
+        if ($td.children().length) {
+            $td.text($td.children()[0].innerHTML);
+        }
+        $td.text($td.text().trim());
+    });
+    $('#egtable').find('thead').find('tr:last').append('<th>select <input type="checkbox" id="selectAll" /></th>');
+    $('#egtable').find('tbody').find('tr').each(function (i, tr) {
+        var $tr = $(tr);
+        $tr.append('<th><input type="checkbox" name="interfaceCheck" /></th>');
+    });
+    $('#selectAll').change(function() {
+        var $all = $(this).attr('checked');
+        $('input[name=interfaceCheck]').each(function() {
+            $(this).attr('checked', $all);
+        });
+    });
+
+
+}
+
+
 function enableEditableGrid() {
     var $eg = $('#eg');
     if (!$eg) {
         return;
     }
+
+    //Remove batch update mode
+    if ($('#enable-batch-update').length) {
+        $('#enable-batch-update').remove()
+    };
 
     // Remove Action column.
     if ($('th:contains("Actions")')) {
@@ -49,6 +93,7 @@ function enableEditableGrid() {
 
 $(document).ready(function() {
     var $enableEg = $('#enable-eg');
+    var $enableBatch = $('#enable-batch-update');
     if ($enableEg.length) {
         $enableEg[0].reset();
 
@@ -64,4 +109,16 @@ $(document).ready(function() {
             $('.spreadsheet-mode').show();
         });
     }
+    if ($enableBatch.length) {
+        $enableBatch[0].reset();
+        $enableBatch.find('input').removeAttr('disabled').change(function() {
+            $this = $(this);
+            if ($this.attr('checked')) {
+                enableBatchUpdate();
+            } else {
+                location.reload();
+            }
+        });
+    }
+
 });
