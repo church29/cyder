138be462a6488726c0ea94330d676f64716e7235
church29 <joshchurch29@gmail.com>
2013-11-27 15:49:38 -0800

    Added range type selector for batch update form

--- cyder/cydhcp/interface/forms.py
+++ cyder/cydhcp/interface/forms.py
@@ -5,4 +5,9 @@ from cyder.base.mixins import UsabilityFormMixin
 
 
 class BatchInterfaceForm(forms.Form, UsabilityFormMixin):
+    range_type = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        choices=(
+            ('static', 'Static'),
+            ('dynamic', 'Dynamic')))
     range = forms.ModelChoiceField(queryset=Range.objects.all())
