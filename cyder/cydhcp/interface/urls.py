0d4928d957ae6efcc5b6f93a267ba222b53762b1
church29 <joshchurch29@gmail.com>
2013-12-04 09:56:41 -0800

    Added url for get_ranges

--- cyder/cydhcp/interface/urls.py
+++ cyder/cydhcp/interface/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
-from cyder.cydhcp.interface.views import interface_delete
-from cyder.cydhcp.interface.views import batch_update
+from cyder.cydhcp.interface.views import (
+    batch_update, get_ranges, interface_delete)
 
 urlpatterns = patterns(
     '',
     url(r'^interface_delete/', interface_delete, name='interface-delete'),
     url(r'^batch_update/', batch_update, name='batch-update'),
+    url(r'^get_ranges/', get_ranges, name='get_ranges'),
 )
